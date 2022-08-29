95e7b3b60a7284654817d73c6b18f9c3d4a4bb88
LimYenQi <77624799+LimYenQi@users.noreply.github.com>
2022-08-29 15:43:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cth0hhcslakm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limyenqi-employee"
+customregion = "us-east-1"
